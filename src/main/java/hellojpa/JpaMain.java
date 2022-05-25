febd1a32dd21c682be2b17e21e0f65530780e6dc
박상훈 <psh995666@gmail.com>
2022-05-25 23:32:39 +0900

    [MODIFY] JDK11 properties - pom.xml | [ADD] EntityManagerFactory

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,21 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JpaMain {
 
     public static void main(String[] args) {
+        // 시작
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
 
+        // EntityManager를 꺼내고 실제 동작하는 코드를 작성하는 부분(CRUD)
+
+        // 종료
+        em.close();
+        emf.close();
     }
 
 }
