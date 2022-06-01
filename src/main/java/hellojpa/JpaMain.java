fd461ecb844412c3290e26967ea92ccf119bbf99
박상훈 <psh995666@gmail.com>
2022-06-01 16:04:10 +0900

    [EMF·EM·CRUD·SELECTLIST·PAGINATION·JPQL]

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,30 +17,27 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        // 문제가 발생 시, em, emf가 호출이 안될 수 있음 따라서 try~catch 구문
+        // 문제가 발생 시, EntityManagerFactory, EntityManager가 호출이 안될 수 있으므로 try~catch 구문
         try {
 
-            /** 비영속 상태 : 아무것도 아닌 상태 = 객체를 생성 및 set
-             * JPA와 아무 관련 없는 상태로 DB에 들어가지 않음 */
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
+            /** JPQL : 가장 단순한 조회는 EM을 통해 find 메서드 인자에 type과 pk값을 넣으면 해결
+             * 그러나 리스트 조회 또는 JOIN 즉 내가 원하는 데이터를 최적화해서 가져오기 필요한 것 */
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
 
-            /** 영속 상태 : EntityManager 안에 있는 영속성 컨텍스트
-             * 라는 애를 통해 이 member가 관리가 되어짐 */
-            // 해당 메서드를 실행해보면, Query는 BEFORE와 AFTER 사이에 실행되지 않음
-            System.out.println("------------BEFORE------------");
-            em.persist(member); // 객체를 저장한 상태(영속)
-            System.out.println("-------------AFTER------------");
-            // 이 곳에서 INSERT QUERY가 실행됨
-            /** 즉, 영속상태가 된다고 해서 바로 DB에 QUERY가 날라가지 않고
-             * transaction을 commit하는 시점에 영속성 컨텍스트에 있는 애가 DB에 QUERY가 날라감 */
+            /** createQuery의 sql문을 보면 기존 sql문과는 조금 다름
+             * JPA 입장에서는 코드를 짤 때, 테이블 대상으로 코드를 짜지 않고, Member 객체(Entity)를 대상으로 쿼리를 작성
+             * 따라서, JPA의 대상은 테이블이 아닌 객체 */
+            for (Member member : result ) {
+                System.out.println("member = " + member.getId() + ", " + member.getName());
+            }
+
+            // 정상일 때 commit
             tx.commit();
         } catch (Exception e) {
             // 문제가 생기면 rollback
             tx.rollback();
         } finally {
-            // 작업이 다 끝나면 entity manager가 닫아줌
+            // 작업이 다 끝나면 entity manager 종료
             // 내부적으로 Data Connection을 물고 동작하기 때문에 사용 후 꼭 닫아줘야 함
             em.close();
         }
@@ -60,37 +57,7 @@ public class JpaMain {
  * );
  */
 
-/**
- * 주의 사항
- * 1) Entity Manager Factory
- * - Web Application Service 를 운영한다고 했을 때, Web-Server가 올라 오는 시점에 DB당 딱 하나만 생성이 되고 Entity Manager는
- * 2) Entity Manager
- * - 고객의 요청이 올 때마다 계속 썼다가(CRUD), 버렸다가(em.close()) 반복
- * - 따라서 Entity Mangager는 쓰레드 간에 공유를 하면 안 됨(사용하고 버려야 함)
- *   하나를 만들어서 여러 쓰레드에서 같이 사용하면 장애가 발생함
- *   DB 커넥션을 빨리 쓰고 돌려주는 것 처럼
- * 3) JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
- */
-
-/**
- * JPQL 이란?
- * - JPA를 사용하면 Entity 객체를 중심으로 개발
- * - 그런데 문제는 검색 쿼리
- * - 그래서 검색을 할 때도 테이블이 아닌 객체를 대상으로 쿼리를 짤 수 있는 문법
- * - 모든 DB데이터를 객체로 변환해서 검색하는 것은 불가능
- * - 애플리케이션이 필요한 데이터만 DB에서 불러오려면 결국 검색조건이 포함된 SQL을 날려야 함
- * - 실제 RDB에 있는, 물리적인 대상인 쿼리를 날려버리면 그 DB에 종속적으로 설계가 되어버림
- * - 그래서 그게 아니라 Entity 객체를 대상으로 쿼리를 사용할 수 있는 JPQL이란 것이 제공됨
- * 1) JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어를 제공
- * 2) SQL과 문법 유사, SELECT, FROM, WHERE, GROUP BY, HANVING, JOIN 지원
- * 3) 차이점 : JPQL은 엔티티 객체를 대상으로 쿼리(SQL은 DB 테이블을 대상으로 쿼리)
- *    장점 : DB를 바꿔도 JPQL을 변경할 필요없이 그대로 사용해도 실행됨
- *          왜냐하면 SQL을 추상화해서 특정 DB의 SQL에 의존하지 않음
- * 4) JPQL을 한마디로 정의하면 객체 지향 SQL
- */
-
-/**
- * 22-05-29
+/*** 1. 기초 개념
  * 1) H2 DB 사용
  * 2) JPA 라이브러리 세팅
  * 3) persistence.xml
@@ -107,3 +74,65 @@ public class JpaMain {
  *  - 그리고 (WebApplicationService일 경우)WAS가 내려 갈 때, 꼭 EntityManagerFactory를 close() 닫아줘야
  *    내부적으로 커넥션 풀링이나 리소스가 릴리즈 됨
  * */
+
+/*** 2. CRUD 로직(try구문 내부)
+ * 1) INSERT
+ *    Member member = new Member();
+ *    member.setId(100L);
+ *    member.setName("HelloB");
+ *    em.persist(member);
+ * 2) SELECT
+ *    Member findMember = em.find(Member.class, 1L);
+ *    System.out.println("findMember.id = " + findMember.getId());
+ *    System.out.println("findMember.name = " + findMember.getName());
+ * 3) DELETE
+ *    Member findMember = em.find(Member.class, 1L);
+ *    em.remove(findMember);
+ * 4) UPDATE
+ *    Member findMember = em.find(Member.class, 1L);
+ *    findMember.setName("HelloJPA");
+ *    · em.persiste(findMember) 으로 저장하는 것이 아님
+ *    · 자바 객체에서 값만 바꾸었는데 수정이 가능한 것은 자바컬렉션과 같이 다루는 것처럼 설계되어 있어서 그럼
+ *    · JPA를 통해서 Entity를 가져오면 JPA가 관리를 하고, 변경이 되었는지 안 되었는지 트랜잭션을 커밋하는 시점에 체크함
+ *    · 무언가 변경 된 걸 감지하고 UPDATE 쿼리를 실행 후에 트랜잭션이 커밋함
+ * */
+
+/*** 3. JPA 주의사항 3가지
+ * 1) Entity Manager Factory
+ * - Web Application Service 를 운영한다고 했을 때,
+ *   Web-Server가 올라 오는 시점에 EMF가 DB당 딱 하나만 생성이 되고 Entity Manager는
+ * 2) Entity Manager
+ * - Java collection 같은 역할로 객체를 대신 저장해줌
+ * - 고객의 요청이 올 때마다 계속 썼다가(CRUD), 버렸다가(em.close()) 반복
+ * - 따라서 Entity Mangager는 쓰레드 간에 공유를 하면 안 됨(사용하고 버려야 함)
+ *   하나를 만들어서 여러 쓰레드에서 같이 사용하면 장애가 발생(DB 커넥션을 빨리 쓰고 돌려주는 것 처럼)
+ * 3) JPA의 모든 데이터 변경은 트랜잭션 안에서 실행해야 함
+ */
+
+/*** 4. selectList 로직(try구문 내부)
+ * 1) selectList
+ * - List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+ *   for() 문;
+ * 2) selectList(ver. pagination)
+ * - List<Member> result = em.createQuery("select m from Member as m", Member.class)
+ *                           .setFirstResult(1) //1번 데이터부터
+ *                           .setMaxResults(5)  //5개의 데이터를 호출
+ *                           .getResultList();
+ *   for() 문;
+ * */
+
+/*** 5. JPQL 이란?
+ * - JPA를 사용하면 Entity 객체를 중심으로 개발
+ * - 그런데 문제는 검색 쿼리
+ * - 그래서 검색을 할 때도 테이블이 아닌 객체를 대상으로 쿼리를 짤 수 있는 문법
+ * - 모든 DB데이터를 객체로 변환해서 검색하는 것은 불가능
+ * - 애플리케이션이 필요한 데이터만 DB에서 불러오려면 결국 검색조건이 포함된 SQL을 날려야 함
+ * - 실제 RDB에 있는, 물리적인 대상인 쿼리를 날려버리면 그 DB에 종속적으로 설계가 되어버림
+ * - 그래서 그게 아니라 Entity 객체를 대상으로 쿼리를 사용할 수 있는 JPQL이란 것이 제공됨
+ * 1) JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어를 제공
+ * 2) SQL과 문법 유사, SELECT, FROM, WHERE, GROUP BY, HANVING, JOIN 지원
+ * 3) 차이점 : JPQL은 엔티티 객체를 대상으로 쿼리(SQL은 DB 테이블을 대상으로 쿼리)
+ *    장점 : DB를 바꿔도 JPQL을 변경할 필요없이 그대로 사용해도 실행됨
+ *          왜냐하면 SQL을 추상화해서 특정 DB의 SQL에 의존하지 않음
+ * 4) JPQL을 한마디로 정의하면 객체 지향 SQL
+ */
